b3533c791a0f3e83d435b41485b62b93277ef170
BGluth <gluthb@gmail.com>
2019-09-08 09:42:26 -0600

    Init version of rand gen API from Jupyter docs
    - More docs will come, but this is the initial version.

---
+++ random_data_gen.py
@@ -0,0 +1,167 @@
+import numpy as np
+import pandas as pd
+import sklearn as skl
+from sklearn import datasets
+
+# Used to control the range of values for the random gen func (1.0 --> [-1.0, 1.0])
+RAND_FEAT_RANGE = 1.0
+USERS_MIN_UNIQUE_LABELS = 2
+
+
+class InputGenParams:
+    def __init__(self, num_samples, num_labels, num_features, num_users):
+        self.num_samples = num_samples
+        self.num_labels = num_labels
+        self.num_features = num_features
+        self.num_users = num_users
+
+
+def generate_blob_data(g_prms):
+    """
+    Generates blob data, or data points that tend to cluster around a point for each label.
+    Note that labels/species are interchangeable.
+    Returns: The features and labels generated (see file header for format)
+    """
+    df = _gen_data_and_add_user_data(_gen_blob_data, g_prms)
+    return _transform_data_for_simulator_format(df, g_prms)
+
+
+def generate_random_data(g_prms):
+    """
+    Generates random data where features should have no correlation.
+    Note that labels/species are interchangeable.
+    Returns: The features and labels generated (see file header for format)
+    """
+    df = _gen_data_and_add_user_data(_gen_random_data, g_prms)
+    return _transform_data_for_simulator_format(df, g_prms)
+
+
+def _gen_blob_data(g_prms):
+    feat_arr, label_idxs = skl.datasets.make_blobs(
+        n_samples=g_prms.num_samples,
+        n_features=g_prms.num_features,
+        centers=g_prms.num_labels,
+    )
+
+    df = pd.DataFrame(feat_arr)
+    _evenly_add_labels_to_data(df, g_prms)
+    return df
+
+
+def _gen_random_data(g_prms):
+    df = pd.DataFrame()
+
+    for i in range(0, g_prms.num_features):
+        feat_arr = np.random.ranf(g_prms.num_samples) * RAND_FEAT_RANGE
+        df[i] = feat_arr
+
+    _evenly_add_labels_to_data(df, g_prms)
+    return df
+
+
+def _evenly_add_labels_to_data(df, g_prms):
+    _add_evenly_distributed_values_to_data(df, g_prms, g_prms.num_labels, "labels")
+
+
+def _add_users_ids_to_data_and_shuffle(df, g_prms):
+    df = skl.utils.shuffle(df)
+    _add_evenly_distributed_values_to_data(df, g_prms, g_prms.num_users, "user_id")
+    return df
+
+
+def _all_users_have_at_least_n_unique_lables(data):
+    nunique_labels = data.groupby("user_id", as_index=False).agg({"labels": "nunique"})
+    ok = (nunique_labels["labels"] < USERS_MIN_UNIQUE_LABELS).sum() == 0
+
+    if not ok:
+        print(
+            "Not all users have {} unique labels! Regenerating!".format(
+                USERS_MIN_UNIQUE_LABELS
+            )
+        )
+    return ok
+
+
+def _gen_data_until_prereq_met(data_gen_func, prereq_func):
+    prereq_met = False
+    df = None
+
+    while not prereq_met:
+        df = data_gen_func()
+        prereq_met = prereq_func(df)
+
+    return df
+
+
+def _gen_data_and_add_user_data(data_gen_func, g_prms):
+    def gen_and_add_users_func():
+        data = data_gen_func(g_prms)
+        data = _add_users_ids_to_data_and_shuffle(data, g_prms)
+        return data
+
+    return _gen_data_until_prereq_met(
+        gen_and_add_users_func, _all_users_have_at_least_n_unique_lables
+    )
+
+
+def _transform_data_for_simulator_format(df, g_prms):
+    labels = []
+    feats = []
+    for i in range(g_prms.num_users):
+        client_df = df[df.user_id == i]
+        labels.append(list(client_df.labels))
+        client_feats = client_df.drop(columns=["user_id", "labels"]).to_records(
+            index=False
+        )
+        feats.append(list(client_feats))
+
+    return (labels, feats)
+
+
+def _gen_data_and_add_user_data(data_gen_func, g_prms):
+    def gen_and_add_users_func():
+        data = data_gen_func(g_prms)
+        data = _add_users_ids_to_data_and_shuffle(data, g_prms)
+        return data
+
+    return _gen_data_until_prereq_met(
+        gen_and_add_users_func, _all_users_have_at_least_n_unique_lables
+    )
+
+
+def _add_users_ids_to_data_and_shuffle(df, g_prms):
+    df = skl.utils.shuffle(df)
+    _add_evenly_distributed_values_to_data(df, g_prms, g_prms.num_users, "user_id")
+    return df
+
+
+def _gen_data_until_prereq_met(data_gen_func, prereq_func):
+    prereq_met = False
+    df = None
+
+    while not prereq_met:
+        df = data_gen_func()
+        prereq_met = prereq_func(df)
+
+    return df
+
+
+def _all_users_have_at_least_n_unique_lables(data):
+    nunique_labels = data.groupby("user_id", as_index=False).agg({"labels": "nunique"})
+    ok = (nunique_labels["labels"] < USERS_MIN_UNIQUE_LABELS).sum() == 0
+
+    if not ok:
+        print(
+            "Not all users have {} unique labels! Regenerating!".format(
+                USERS_MIN_UNIQUE_LABELS
+            )
+        )
+    return ok
+
+
+def _add_evenly_distributed_values_to_data(df, g_prms, num_items, field_name):
+    num_samples_per_value = g_prms.num_samples // num_items
+
+    new_field = [i // num_samples_per_value for i in range(0, g_prms.num_samples)]
+    df[field_name] = new_field
+
