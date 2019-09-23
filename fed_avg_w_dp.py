e08b93cae62bb7131abee87140d4f1bce738ab10
BGluth <gluthb@gmail.com>
2019-09-23 17:08:06 -0600

    WIP (SQUASH)

--- fed_avg_w_dp.py
+++ fed_avg_w_dp.py
@@ -1,3 +1,7 @@
+class FedAvgWithDpParams:
+    def __init__(self):
+        pass
+
 def fed_avg_w_dp():
     pass
 
@@ -6,5 +10,13 @@ def flat_clip():
     pass
 
 
-def user_update_fed_avg():
-    pass
+def user_update_fed_avg(params):
+
+
+    for round_t in range(params.num_rounds):
+        pass
+
+
+
+def _get_random_selection_of_user_idxs(num_users):
+    S = np.array(random.sample(range(client_num), client_num))
