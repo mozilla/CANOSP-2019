058b986d0b2d08d6e96790faa2fe6946b917fe8f
BGluth <gluthb@gmail.com>
2019-11-07 20:47:25 -0700

    Added comments... Probably too many...
    - All functions (except TODO ones) now have doc strings.

--- mozfldp/fed_avg_w_dp.py
+++ mozfldp/fed_avg_w_dp.py
@@ -4,6 +4,10 @@ from sklearn.linear_model import SGDClassifier
 
 
 class FedAvgWithDpParams:
+    """
+    Used just to hold parameters needed to run FedAvgWithDP
+    """
+
     def __init__(
         self,
         num_users,
@@ -34,6 +38,7 @@ class FedAvgWithDpParams:
 def run_fed_avg_with_dp(prms, data):
     """
     Runs federated averaging with differential privacy
+
     prms: The parameters needed to run (FedAvgWithDpParams)
     data: Data to train on. In the format of: ([user_1_labels, user_2_labels, ...], [user_1_feats, user_2_feats, ...])
     """
@@ -101,6 +106,12 @@ def _merge_all_user_thetas(
 ):
     """
     Merge all user updates for a round into a single delta (vector).
+
+    user_sel_prob: Probability of any given user being selected for a round
+    weight_sum: The sum of all user weights
+    user_updates_buf: The user updates (thetas) that we are merging.
+    user_weights: The weights applied to each user. Users with more data have more weight.
+    num_theta_elems: The number of elements in theta.
     """
 
     num_users_in_batch = len(user_updates_buf)
@@ -115,6 +126,11 @@ def _merge_all_user_thetas(
 
 
 def flat_clip(sensitivity, vec):
+    """
+    "Clips" a vector, in order to limit how "long" a vector can be.
+
+    sensitivity: Affects how long a vector can be. Higher values --> longer vector.
+    """
     norm = np.linalg.norm(vec)
     if norm > sensitivity:
         vec *= sensitivity / norm
@@ -123,6 +139,15 @@ def flat_clip(sensitivity, vec):
 
 
 def user_update_fed_avg(prms, round_user_features, round_user_labels, theta_0):
+    """
+    Calculates an update to theta (coefs, intercepts) based on data sampled from a single user.
+    Does not update theta directly.
+    These updates from individual users are then merged into a single update before performing updating theta.
+
+    round_user_features/labels: The data from the user that are using to calculate the update.
+    theta_0: The current weights of theta from the previous round.
+    Note that other user updates before this update do not affect theta_0!
+    """
 
     batches_features = []
     batches_labels = []
@@ -140,6 +165,7 @@ def user_update_fed_avg(prms, round_user_features, round_user_labels, theta_0):
         batches_features.append(round_user_features[i : i + prms.batch_size])
         batches_labels.append(round_user_labels[i : i + prms.batch_size])
 
+    # Train with the batches
     for _ in range(prms.num_epochs):
         for j in range(num_batches):
 
@@ -161,6 +187,13 @@ def user_update_fed_avg(prms, round_user_features, round_user_labels, theta_0):
 
 
 def _get_random_selection_of_user_idxs(num_users, user_sel_prob):
+    """
+    Selects a random set of user idxs based on the chances of picking any given user.
+
+    num_users: The total number of users in the simulation.
+    user_sel_prob: The chance of picking any given user
+    """
+
     # This is definitely not the optimal way to do this, but it gets the job done
     # Very dumb brute force impl...
 
@@ -175,6 +208,12 @@ def _get_random_selection_of_user_idxs(num_users, user_sel_prob):
 
 
 def _init_theta(num_features, num_labels):
+    """
+    Initializes theta.
+    Currently this just means initializing all weights to 0.
+
+    Returns: Inited theta
+    """
     # Just a placeholder value for now
 
     # theta is internally a linear array. We will use a functon to get slices of the features/labels.
@@ -182,11 +221,17 @@ def _init_theta(num_features, num_labels):
 
 
 def _init_user_weights_and_weight_sum(data, num_users, user_weight_cap):
+    """
+    Inits all user weights and also returns the sum of all user weight.
+
+    returns: A tuple containing the calculated weights for all users and the summed weight
+    """
+
     user_weights = []
     weight_sum = 0
 
     for usr_idx in range(num_users):
-        user_weight =_init_user_weight(data, usr_idx, user_weight_cap)
+        user_weight = _init_user_weight(data, usr_idx, user_weight_cap)
         user_weights.append(user_weight)
         weight_sum += user_weight
 
@@ -194,18 +239,35 @@ def _init_user_weights_and_weight_sum(data, num_users, user_weight_cap):
 
 
 def _init_user_weight(data, usr_idx, user_weight_cap):
+    """
+    Inits a user's weight based on the amount of entries they have and the weight cap param.
+
+    data: The entire dataset passed into the sim
+    usr_idx: The user idx/id
+    user_weight_cap: Used to limit the maximum amount of weight that a user can have
+
+    returns: The calculated user weight
+    """
+
     user_labels, _ = _get_data_for_user(data, usr_idx)
     num_user_entries = len(user_labels)
     return min(num_user_entries / user_weight_cap, 1)
 
 
 def _calc_standard_dev(noise_scale, sensitivity, usr_sel_prob, weight_sum):
+    """
+    Calculates the standard deviation
+    Parameters are tailored to this sim impl
+    """
     return (noise_scale * sensitivity) / (usr_sel_prob * weight_sum)
 
 
 def _get_data_for_user(data, user_id):
     """
-    Pick all the labels/feature that belong to a user for a round
+    Pick all the labels/feature that belong to a user for a round.
+
+    data: The entire dataset passed into the sim
+    user_id: The user idx to extract data for
     """
 
     labels, feats = data
@@ -213,6 +275,15 @@ def _get_data_for_user(data, user_id):
 
 
 def _gen_gausian_rand_noise(stndrd_dev, vec_len):
+    """
+    Generates gausian noise and applies to all elements in a vector.
+
+    stndrd_dev: The standard deviation of the distrubution to sample from
+    vec_len: The number of elements in the vector
+
+    returns: The vector after noise has been applied
+    """
+
     return np.random.normal(loc=0.0, scale=stndrd_dev, size=vec_len)
 
 
@@ -225,6 +296,12 @@ def _calc_privacy_spent():
 
 
 def _get_coef_and_inter_slice_from_theta(theta, num_features, num_labels):
+    """
+    Helper function to extract slices of the coefs/intercepts of theta (which is stored as a 1D array)
+
+    returns: A tuple containing slices of the coefs and intercepts (coefs, intercepts)
+    """
+
     coef_len = num_features * num_labels
     coef_arr_slices = []
 
@@ -234,12 +311,20 @@ def _get_coef_and_inter_slice_from_theta(theta, num_features, num_labels):
     return coef_arr_slices, theta[coef_len:]
 
 
-def _set_coef_and_inter_on_theta(theta, coef_arr_slices, inter):
-    num_features = len(coef_arr_slices[0])
+def _set_coef_and_inter_on_theta(theta, coef_arrs, inter):
+    """
+    Helper function to update the coefs/intercepts of a theta.
+    coef_arrs & inter are the coeficients/intercepts that the SGDClassifier outputs.
+
+    coef_arr_slices: An array of arrays containing the coefs
+    inter: An array containing the intercepts
+    """
+
+    num_features = len(coef_arrs[0])
     coef_len = num_features * len(inter)
 
-    for i, slice in enumerate(coef_arr_slices):
+    for i, slice in enumerate(coef_arrs):
         coef_start_idx = i * num_features
-        theta[coef_start_idx : coef_start_idx + num_features] = coef_arr_slices[i]
+        theta[coef_start_idx : coef_start_idx + num_features] = coef_arrs[i]
 
     theta[coef_len:] = inter
