7b3203f847b76c573569d48733ccb5b9466afb1f
Dave Zeber <dzeber@mozilla.com>
2019-11-13 13:38:01 -0600

    Implement batching of client data

--- mozfldp/client.py
+++ mozfldp/client.py
@@ -2,6 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import numpy as np
+
 
 class Client:
     """A client which trains model updates on its personal dataset for FL.
@@ -9,26 +11,33 @@ class Client:
     client_id: a unique ID to assign each client
     features: an array of shape (`n_examples`, `n_features`) containing the
         client's personal dataset features
-    labels: an array of shape (`n_features`) containing the client's personal
+    labels: an array of shape (`n_examples`) containing the client's personal
         dataset labels
     model: a `SGDModel` instance to be used for training
     """
 
     def __init__(self, client_id, features, labels, model):
+        if not features.shape[0] == len(labels):
+            raise ValueError(
+                "Features and labels have incompatible shapes for client {}".format(
+                    client_id
+                )
+            )
+
         self._id = client_id
         self._features = features
         self._labels = labels
         self._model = model
+        self._n = len(labels)
 
     def _get_batch_indices(self, batch_size):
         """Randomly split data into minibatches of target size `batch_size`.
 
-        Returns a list `[(i_1_1, i_1_2, ..., i_1_b), (i_2_1, ..., i_2_b), ...]`
-        of length `len(features) // batch_size` containing index sets for each
-        batch.
+        Returns a list of length `ceiling(self._n / batch_size)` containing
+        index lists for each batch.
         """
-        # TODO
-        pass
+        shuffled_ind = np.random.permutation(self._n)
+        return [shuffled_ind[i : i + batch_size] for i in range(0, self._n, batch_size)]
 
     def _run_model_update_step(self, X, y):
         """Run a single GD update step on the given data minibatch."""
--- tests/test_client.py
+++ tests/test_client.py
@@ -3,12 +3,46 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import pytest
+import numpy as np
+
+from mozfldp.client import Client
+
+
+def check_batch_indices(batch_ind, data_n, main_batch_size, final_batch_size):
+    # batch_ind is a list containing index lists.
+    all_ind = np.concatenate(batch_ind)
+    # indices in batch_ind should cover 0, 1, ..., n-1.
+    assert len(np.setxor1d(all_ind, np.arange(data_n))) == 0
+    # number of batches:
+    assert len(batch_ind) == np.ceil(data_n / main_batch_size)
+    # all index lists should be of the same size, except possibly the last one.
+    for i in range(len(batch_ind) - 1):
+        assert len(batch_ind[i]) == main_batch_size
+    assert len(batch_ind[-1]) == final_batch_size
 
 
 def test_batching():
-    # TODO: batching works as expected:
-    # right number of batches of the right sizes
-    pass
+    features = np.array(
+        [
+            [6, 9, 6],
+            [9, 2, 8],
+            [0, 5, 4],
+            [2, 6, 1],
+            [6, 8, 1],
+            [6, 1, 6],
+            [5, 8, 5],
+            [7, 6, 6],
+        ]
+    )
+    labels = np.array([1, 0, 1, 0, 1, 0, 1, 0])
+
+    client = Client(client_id=None, features=features, labels=labels, model=None)
+
+    batches_size_div = client._get_batch_indices(2)
+    check_batch_indices(batches_size_div, 8, 2, 2)
+
+    batches_size_nondiv = client._get_batch_indices(3)
+    check_batch_indices(batches_size_nondiv, 8, 3, 2)
 
 
 def test_update_weights():
