4b6ce753086ad5f8b19cb42d4ea2bf77124e6ec9
Maharsh Patel <patel@maharsh.net>
2019-11-19 17:51:27 -0700

    cleanup

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,5 +1,5 @@
 import pytest
-import numpy as np 
+import numpy as np
 import json
 
 from mozfldp.server import ServerFacade
@@ -10,31 +10,28 @@ CLIENT_FRACTION = 0.5
 NUM_CLIENTS = 10
 NUM_SAMPLES = 10
 
+
 @pytest.fixture
 def coef():
     return np.zeros((NUM_LABELS, NUM_FEATURES), dtype=np.float64, order="C")
 
-# @pytest.fixture
-# def random_coef():
-#     return np.random.rand(NUM_LABELS, NUM_FEATURES)
 
 @pytest.fixture
 def intercept():
     return np.zeros(NUM_LABELS, dtype=np.float64, order="C")
 
-# @pytest.fixture
-# def random_intercept():
-#     return np.random.rand(NUM_LABELS)
 
 @pytest.fixture
 def server(coef, intercept):
-    return ServerFacade(coef, intercept, num_client=NUM_CLIENTS, client_fraction=CLIENT_FRACTION)
+    return ServerFacade(
+        coef, intercept, num_client=NUM_CLIENTS, client_fraction=CLIENT_FRACTION
+    )
 
 
 def test_server_initialization(server, coef, intercept):
     np.testing.assert_array_equal(server._coef, coef)
     np.testing.assert_array_equal(server._intercept, intercept)
-    
+
     assert server._num_client == NUM_CLIENTS
     assert server._client_fraction == CLIENT_FRACTION
 
@@ -42,13 +39,14 @@ def test_server_initialization(server, coef, intercept):
     assert len(server._client_intercepts) == 0
     assert len(server._num_samples) == 0
 
+
 def test_injest(server, coef, intercept):
     # send weights from the client to the server
     payload = {
         "coefs": coef.tolist(),
         "intercept": intercept.tolist(),
-        "num_samples": 5
-    };
+        "num_samples": 5,
+    }
     server.ingest_client_data(json.dumps(payload))
 
     # check that the payload got injested
@@ -60,6 +58,7 @@ def test_injest(server, coef, intercept):
     np.testing.assert_array_equal(server._client_coefs[0], coef)
     np.testing.assert_array_equal(server._client_intercepts[0], intercept)
     assert server._num_samples[0] == 5
+
 
 def test_compute_new_weights(server, coef, intercept):
     # client 1 coefs and intercepts matrices are filled with the value 1
@@ -85,8 +84,8 @@ def test_compute_new_weights(server, coef, intercept):
 
     new_coef, new_intercept = server.compute_new_weights()
 
-    # since client 1 has 4 samples and client 2 has 6 samples, the update 
-    # from client 2 is weighted more - the weighted average between 1 and 2 
+    # since client 1 has 4 samples and client 2 has 6 samples, the update
+    # from client 2 is weighted more - the weighted average between 1 and 2
     # is 1.6
     expected_coef = np.copy(coef)
     expected_intercept = np.copy(intercept)
@@ -96,4 +95,3 @@ def test_compute_new_weights(server, coef, intercept):
     np.testing.assert_array_equal(new_coef, expected_coef)
     np.testing.assert_array_equal(new_intercept, expected_intercept)
 
-    
