8f76e6582b29680e1103345d16fb679b4c14625b
Yuxuan Zhao <me@yuxuanz.com>
2019-11-24 13:07:01 -0700

    update the test to test if payload has been passed correctly

--- tests/test_client.py
+++ tests/test_client.py
@@ -10,6 +10,7 @@ from mozfldp.client import Client
 from mozfldp.model import SGDModel
 
 import random
+import json
 
 FEATURES = [
     [6, 9, 6],
@@ -97,13 +98,14 @@ def test_update_weights(client, monkeypatch, batched_indices_2):
 
     monkeypatch.setattr(client, "_run_model_update_step", mock_model_update)
 
-    reset_random_seed()
     # mock the post request 
     mock_post_patcher = patch('mozfldp.client.requests.post')
     mock_post = mock_post_patcher.start()
     mock_post.return_value.ok = True
+
+    reset_random_seed()
     client.update_and_submit_weights(
-        current_coef=None, current_intercept=None, num_epochs=1, batch_size=2
+        current_coef=np.array([]), current_intercept=np.array([]), num_epochs=1, batch_size=2
     )
     assert len(model_update_data) == len(batched_indices_2)
     for (feat, lab), exp_ind in zip(model_update_data, batched_indices_2):
@@ -113,7 +115,7 @@ def test_update_weights(client, monkeypatch, batched_indices_2):
     reset_random_seed()
     model_update_data = []
     client.update_and_submit_weights(
-        current_coef=None, current_intercept=None, num_epochs=3, batch_size=2
+        current_coef=np.array([]), current_intercept=np.array([]), num_epochs=3, batch_size=2
     )
     batched_ind = batched_indices_2 * 3
     print(batched_ind)
@@ -121,7 +123,26 @@ def test_update_weights(client, monkeypatch, batched_indices_2):
     for (feat, lab), exp_ind in zip(model_update_data, batched_ind):
         assert np.array_equal(feat, client._features[exp_ind])
         assert np.array_equal(lab, client._labels[exp_ind])
-
+    
+    reset_random_seed()
+    model_update_data = []
+    coefs = np.array([29., 0., 0.])
+    intercepts = np.array([-9.])
+    expected_payload = {
+        "coefs": coefs.tolist(),
+        "intercept": intercepts.tolist(),
+        "num_samples": len(LABELS),
+    }
+    expected_clientId = None
+    expected_url = "http://0.0.0.0:8000/api/v1/ingest_client_data/{}".format(expected_clientId)
+    client.update_and_submit_weights(
+        current_coef=coefs, current_intercept=intercepts, num_epochs=3, batch_size=2
+    )
+    batched_ind = batched_indices_2 * 3
+    print(batched_ind)
+    assert len(model_update_data) == len(batched_ind)
+    mock_post.assert_called_with(json=json.dumps(expected_payload), url=expected_url)
+    
     # stop mocking the request
     mock_post_patcher.stop()
 
